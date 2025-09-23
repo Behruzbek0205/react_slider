fbbe954c5b693fa98bdf169bfbc4ce2bc9c37856
Behruzbek0205 <bexruzbekhasanbayev@gmail.com>
2025-09-23 19:58:31 +0500

    nmagap

--- src/components/cummo/Cummo.jsx
+++ src/components/cummo/Cummo.jsx
@@ -11,14 +11,14 @@ import Bag from '../../assets/Bag.svg';
 import User from '../../assets/User.svg';
 import Moon from '../../assets/moon.svg';
 import LogOut from '../../assets/Logout.svg';
-import On from '../../assets/on.svg';
 import One from '../../assets/one.svg';
 import { MdArrowForwardIos } from "react-icons/md";
 
 const Cummo = () => {
   const [open, setOpen] = useState(true);
+  const [turn, setTrun] = useState('white')
   return (
-    <div className="container  max-w-[1540px] m-auto relative h-screen bg-[#485967]">
+    <div className={`container  max-w-[1540px] m-auto relative h-screen `} style={{ backgroundColor: turn }}>
       <div className="slider">
         <div className={` w-[350px]  h-[650px] rounded-r-[10px] absolute top-21 left-[-350px] bg-[#1E252B] p-[20px] transform transition-transform duration-500 ${open ? "-translate-x-0" : "translate-x-full"}`}>
           <div className="chqish w-30px h-[70px] bg-[#1E252B] absolute  left-[332px] top-[290px] rounded-3xl z-[99]">
@@ -68,7 +68,16 @@ const Cummo = () => {
           </div>
           <hr className='w-[290px] relative top-2 text-[grey]' />
           <div className="bottom flex flex-col gap-4 mt-[20px]">
-            <div className="icon flex items-center gap-[12px] pl-4"><img src={Moon} alt="ligth mode" className='w-[25px]' /><h4 className='text-[17.25px] text-[#EFF2F4] font-normal'>Light Mode</h4> <img src={On} alt="on" className='ml-[40px]' /></div>
+            <div className="icon flex items-center gap-[12px] pl-4"><img src={Moon} alt="ligth mode" className='w-[25px]' /><h4 className='text-[17.25px] text-[#EFF2F4] font-normal'>Light Mode</h4> 
+            
+            
+            <div className="btn flex gap-2">
+            <button className='w-[20px] h-[20px] rounded-full bg-[white]' onClick={() => setTrun('white')}></button>
+            <button className='w-[20px] h-[20px] rounded-full bg-[#485967]' onClick={ ()=> setTrun('#485967')}></button>
+            </div>
+            
+            
+             </div>
             <div className="icon flex items-center gap-[12px] pl-4 w-[268px] h-[51.75px] bg-[#667A8A] rounded-[5px]"><img src={LogOut} alt="Logout" className='w-[25px]' /><h4 className='text-[17.25px] text-[#EFF2F4] font-normal'>Logout</h4></div>
           </div>
         </div>
